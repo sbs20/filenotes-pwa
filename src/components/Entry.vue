8360c840b492c692375a23b74f1d23681b09ea6a
Sam Strachan <sam.strachan@gmail.com>
2020-11-24 11:28:05 +0000

    Split up File / Entry + ctrl-s

---
+++ src/components/Entry.vue
@@ -0,0 +1,178 @@
+<template>
+  <div class="container">
+    <prism-editor v-if="type === 'text'" class="editor" v-model="text"
+      :highlight="highlighter" :line-numbers="false"></prism-editor>
+    <av-waveform v-if="type === 'audio'" canv-class="audio-canvas"
+      :audio-src="audioSrc" :canv-top="true" :audio-controls="false"></av-waveform>
+    <audio class="audio-control" v-if="type === 'audio'" controls :src="audioSrc"></audio>
+    <img v-if="imageSrc" :src="imageSrc" />
+  </div>
+</template>
+
+<script>
+import Convert from '../classes/utils/convert';
+import FilePath from '../classes/files/file-path';
+import FileMetadata from '../classes/files/file-metadata';
+import { LocalProvider, Log } from '../services';
+import { PrismEditor } from 'vue-prism-editor';
+
+import 'vue-prism-editor/dist/prismeditor.min.css';
+
+// import highlighting library
+import { highlight, languages } from 'prismjs/components/prism-core';
+import 'prismjs/components/prism-json';
+import 'prismjs/components/prism-markup';
+import 'prismjs/components/prism-markdown';
+import 'prismjs/themes/prism-tomorrow.css';
+
+const log = Log.get('File');
+
+export default {
+  name: 'Entry',
+  props: {
+    /** @type {Metadata} */
+    value: Object
+  },
+
+  components: {
+    PrismEditor
+  },
+
+  data() {
+    return {
+      /** @type {FileType} */
+      type: 'unknown',
+
+      /** @type {string} */
+      text: null,
+
+      audioSrc: null,
+      imageSrc: null
+    };
+  },
+
+  created() {
+    this.load();
+    document.addEventListener('keydown', this._onKeys);
+  },
+
+  destroyed() {
+    this.release();
+    document.removeEventListener('keydown', this._onKeys);
+  },
+
+  watch: {
+    value() {
+      this.load();
+    }
+  },
+
+  methods: {
+    _onKeys(event) {
+      if (event.keyCode === 83 /* s */ && event.ctrlKey) {
+        this.save();
+        event.preventDefault();
+      }
+    },
+
+    highlighter(code) {
+      const language = languages.md;
+      return highlight(code, language);
+    },
+
+    release() {
+      if (this.audioSrc) {
+        URL.revokeObjectURL(this.audioSrc);
+      }
+      if (this.imageSrc) {
+        URL.revokeObjectURL(this.imageSrc);
+      }
+    },
+
+    load() {
+      /** @type {Metadata} */
+      const metadata = this.value;
+      if (metadata && metadata.key) {
+        const type = FilePath.create(metadata.path).type;
+        LocalProvider.read(metadata.key).then(buffer => {
+          this.release();
+          this.text = null;
+          switch (type) {
+            case 'text':
+              this.text = Convert.arrayBufferToString(buffer);
+              break;
+
+            case 'audio': {
+              const blob = Convert.arrayBufferToBlob(buffer);
+              this.audioSrc = window.URL.createObjectURL(blob);
+              break;
+            }
+            case 'image': {
+              const blob = Convert.arrayBufferToBlob(buffer);
+              this.imageSrc = window.URL.createObjectURL(blob);
+              break;
+            }
+          }
+
+          this.type = type;
+        });
+      }
+    },
+
+    save() {
+      if (this.text) {
+        const buffer = Convert.stringToArrayBuffer(this.text);
+        const metadata = FileMetadata.create().assign(this.value).data(buffer).value;
+        LocalProvider.write(metadata, buffer).then((saved) => {
+          if (saved) {
+            log.debug('Saved', this.value);
+            this.$emit('value', metadata);
+          }
+        });
+      } else {
+        log.debug('No text to save');
+      }
+    },
+  }
+};
+</script>
+
+<style scoped>
+/* required class */
+.editor {
+  /* background: #2d2d2d; */
+  color: #ccc;
+  font-family: Fira code, Fira Mono, Consolas, Menlo, Courier, monospace;
+  font-size: 1em;
+  line-height: 1.5em;
+  padding: 0.5em;
+  min-height: 12em;
+  white-space: nowrap;
+  overflow: auto;
+}
+
+/* optional class for removing the outline */
+.prism-editor__textarea:focus {
+  outline: #2d2d2d;
+}
+</style>
+
+<style>
+.prism-editor__editor .title {
+  font-size: 1rem;
+}
+
+.audio-canvas {
+  background: #2d2d2d;
+  color: #ccc;
+  font-family: Fira code, Fira Mono, Consolas, Menlo, Courier, monospace;
+  font-size: 1em;
+  line-height: 1.5em;
+  padding: 0.5em;
+  width: 100%;
+}
+.audio-control {
+  width: 100%;
+}
+</style>
+
--- src/components/File.vue
+++ src/components/File.vue
@@ -10,41 +10,21 @@
       </template>
     </navigation>
 
-    <div class="container">
-      <prism-editor v-if="type === 'text'" class="editor" v-model="text"
-        :highlight="highlighter" :line-numbers="false"></prism-editor>
-      <av-waveform v-if="type === 'audio'" canv-class="audio-canvas"
-        :audio-src="audioSrc" :canv-top="true" :audio-controls="false"></av-waveform>
-      <audio class="audio-control" v-if="type === 'audio'" controls :src="audioSrc"></audio>
-      <img v-if="imageSrc" :src="imageSrc" />
-    </div>
+    <entry ref="entry" v-model="current"></entry>
   </div>
 </template>
 
 <script>
-import Convert from '../classes/utils/convert';
 import FilePath from '../classes/files/file-path';
-import FileMetadata from '../classes/files/file-metadata';
-import { LocalProvider, Log } from '../services';
-import { PrismEditor } from 'vue-prism-editor';
+import { LocalProvider } from '../services';
 
 import Navigation from './Navigation';
-
-import 'vue-prism-editor/dist/prismeditor.min.css';
-
-// import highlighting library
-import { highlight, languages } from 'prismjs/components/prism-core';
-import 'prismjs/components/prism-json';
-import 'prismjs/components/prism-markup';
-import 'prismjs/components/prism-markdown';
-import 'prismjs/themes/prism-tomorrow.css';
-
-const log = Log.get('File');
+import Entry from './Entry';
 
 export default {
   name: 'File',
   components: {
-    PrismEditor,
+    Entry,
     Navigation
   },
 
@@ -52,26 +32,16 @@ export default {
     return {
       /** @type {Metadata} */
       current: {},
-
-      /** @type {FileType} */
-      type: 'unknown',
-
-      /** @type {string} */
-      text: null,
-
-      audioSrc: null,
-      imageSrc: null
     };
   },
 
   created() {
     this.load();
-    document.addEventListener('keyup', this._escape);
+    document.addEventListener('keydown', this._onKeys);
   },
 
   destroyed() {
-    this.release();
-    document.removeEventListener('keyup', this._escape);
+    document.removeEventListener('keydown', this._onKeys);
   },
 
   watch: {
@@ -81,23 +51,9 @@ export default {
   },
 
   methods: {
-    _escape(event) {
+    _onKeys(event) {
       if (event.keyCode === 27) {
         this.close();
-      }
-    },
-
-    highlighter(code) {
-      const language = languages.md;
-      return highlight(code, language);
-    },
-
-    release() {
-      if (this.audioSrc) {
-        URL.revokeObjectURL(this.audioSrc);
-      }
-      if (this.imageSrc) {
-        URL.revokeObjectURL(this.imageSrc);
       }
     },
 
@@ -116,103 +72,16 @@ export default {
           return;
         }
 
-        const type = FilePath.create(path).type;
         this.current = current;
-        this.text = null;
-        LocalProvider.read(this.current.key).then(buffer => {
-          switch (type) {
-            case 'text':
-              this.text = Convert.arrayBufferToString(buffer);
-              break;
-
-            case 'audio': {
-              const blob = Convert.arrayBufferToBlob(buffer);
-              this.audioSrc = window.URL.createObjectURL(blob);
-              break;
-            }
-            case 'image': {
-              const blob = Convert.arrayBufferToBlob(buffer);
-              this.imageSrc = window.URL.createObjectURL(blob);
-              break;
-            }
-          }
-
-          this.type = type;
-        });
       });
-    },
-
-    /**
-     * @param {Metadata} entry
-     */
-    remove() {
-      LocalProvider.delete(this.current.path).then(() => {
-        this.close();
-      });
-    },
-
-    /**
-     * @param {Metadata} entry
-     */
-    rename(entry) {
-      const name = window.prompt('New name');
-      if (name) {
-        const destination = `${this.current.path}/${name}`;
-        LocalProvider.move(entry.key, destination).then(() => {
-          this.close();
-        });
-      }
     },
 
     save() {
-      const buffer = Convert.stringToArrayBuffer(this.text);
-      const metadata = FileMetadata.create().assign(this.current).data(buffer).value;
-      LocalProvider.write(metadata, buffer).then((saved) => {
-        if (saved) {
-          log.debug('Saved', this.current);
-        }
-      });
+      this.$refs.entry.save();
     },
   }
 };
 </script>
 
 <style scoped>
-/* required class */
-.editor {
-  /* background: #2d2d2d; */
-  color: #ccc;
-  font-family: Fira code, Fira Mono, Consolas, Menlo, Courier, monospace;
-  font-size: 1em;
-  line-height: 1.5em;
-  padding: 0.5em;
-  min-height: 12em;
-  white-space: nowrap;
-  overflow: auto;
-}
-
-/* optional class for removing the outline */
-.prism-editor__textarea:focus {
-  outline: #2d2d2d;
-}
 </style>
-
-<style>
-.prism-editor__editor .title {
-  font-size: 1rem;
-}
-
-.audio-canvas {
-  background: #2d2d2d;
-  color: #ccc;
-  font-family: Fira code, Fira Mono, Consolas, Menlo, Courier, monospace;
-  font-size: 1em;
-  line-height: 1.5em;
-  padding: 0.5em;
-  width: 100%;
-}
-.audio-control {
-  width: 100%;
-}
-</style>
-
