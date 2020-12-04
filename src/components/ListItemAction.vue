e3bc9cb773f2104258fa89efaf232767185b4286
Sam Strachan <sam.strachan@gmail.com>
2020-12-04 13:31:04 +0000

    Revert mobilde listactionitem for now

--- src/components/ListItemAction.vue
+++ src/components/ListItemAction.vue
@@ -1,5 +1,5 @@
 <template>
-  <b-dropdown class="pt-2" :mobile-modal="true" position="is-bottom-left" aria-role="list">
+  <b-dropdown class="pt-2" :mobile-modal="false" position="is-bottom-left" aria-role="list">
     <b-icon icon="dots-vertical" slot="trigger"></b-icon>
     <b-dropdown-item aria-role="listitem" @click="$emit('rename')">Rename</b-dropdown-item>
     <b-dropdown-item aria-role="listitem" @click="$emit('move')">Move</b-dropdown-item>
