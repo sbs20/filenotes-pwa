9b1539b65b778fa1dcb0edf8f2aef71f70e0a373
Sam Strachan <sam.strachan@gmail.com>
2021-02-22 12:28:17 +0000

    Todo formatting

--- src/components/editors/TodoTxtTask.vue
+++ src/components/editors/TodoTxtTask.vue
@@ -9,13 +9,13 @@
     <div class="task-body pl-2" @click="$emit('open', task)" :style="{ 'text-decoration': task.isComplete ? 'line-through' : ''}">
       <div class="text-h6">
         {{ task.body }}
-        <span class="secondary">
+        <span class="context mr-1">
           {{ task.contexts.map(p => `@${p}`).join(' ') }}
         </span>
-        <span class="secondary">
+        <span class="project mr-1">
           {{ task.projects.map(p => `+${p}`).join(' ') }}
         </span>
-        <span class="secondary">
+        <span class="field mr-1">
           {{ Object.keys(task.fields).map(f => `${f}:${task.fields[f]}`).join(' ') }}
         </span>
       </div>
@@ -85,4 +85,15 @@ export default {
 div.v-input__slot {
   margin-bottom: 0;
 }
+
+.field {
+  background-color: #b08080;
+}
+.context {
+  background-color: #80b080;
+}
+.project {
+  background-color: #8080b0;
+}
+
 </style>
