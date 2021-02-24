a8b2f31e79e3adab9128cba8dc8d31f9a176ab4c
Sam Strachan <sam.strachan@gmail.com>
2021-02-24 20:31:24 +0000

    TodoTxt editing and new

--- src/components/editors/TodoTxtEditor.vue
+++ src/components/editors/TodoTxtEditor.vue
@@ -1,13 +1,28 @@
 <template>
   <div>
-    <v-dialog v-model="edit.show" v-on:keydown="_onKeys" fullscreen aria-role="dialog"  aria-modal>
+    <v-btn @click="add" color="blue darken-2" fixed fab bottom right dark>
+      <v-icon>mdi-plus</v-icon>
+    </v-btn>
+
+    <v-dialog v-model="editShow" v-on:keydown="_onKeys" fullscreen aria-role="dialog"  aria-modal>
       <v-card>
         <v-card-title>Edit task</v-card-title>
         <v-card-text>
-          <v-textarea v-model="edit.text"></v-textarea>
+          <v-textarea v-model="editText" rows="2"></v-textarea>
         </v-card-text>
 
         <v-card-actions>
+          <v-menu class="pt-2" top offset-y aria-role="list">
+            <template v-slot:activator="{ on }">
+              <v-btn v-on="on">Priority</v-btn>
+            </template>
+            <v-list>
+              <v-list-item v-for="item in ['A', 'B', 'C', 'D']" :key="item" @click="editPriority(item)">{{item}}</v-list-item>
+            </v-list>
+          </v-menu>
+          <v-btn @click="editComplete">
+            {{editText && !editText.startsWith('x ') ? 'Complete' : 'Uncomplete'}}
+          </v-btn>
           <v-spacer></v-spacer>
           <v-btn text @click.prevent="editCancel">Cancel</v-btn>
           <v-btn color="primary" text @click.prevent="editOk">Ok</v-btn>
@@ -21,7 +36,7 @@
 </template>
 
 <script>
-import { TaskList } from 'todo-txt-ts';
+import { Task, TaskList } from 'todo-txt-ts';
 import TodoTxtTask from './TodoTxtTask.vue';
 import Constants from '@/classes/constants';
 import Application from '@/classes/application';
@@ -41,11 +56,9 @@ export default {
 
   data() {
     return {
-      edit: {
-        show: false,
-        task: null,
-        text: null
-      },
+      editShow: false,
+      editTask: null,
+      editText: null,
       taskList: {
         items: []
       }
@@ -55,38 +68,52 @@ export default {
   methods: {
     _onKeys(event) {
       if (event.keyCode === Constants.Keys.escape) {
-        // We need to set this to false in the future otherwise the
-        // document handlers will see it immediately - even with
-        // $nextTick()
+        // We need to set this to false in the future otherwise the document
+        // handlers will see it immediately - even with $nextTick()
         window.setTimeout(() => {
           Application.isDialogActive = false;
         }, 100);
       }
     },
 
+    add() {
+      this.editOpen(new Task());
+    },
+
     editCancel() {
-      this.edit = {
-        show: false,
-        task: null,
-        text: null
-      };
-      Application.isDialogActive = this.edit.show;
+      this.editShow = false;
+      this.editTask = null;
+      this.editText = null;
+      Application.isDialogActive = this.editShow;
+    },
+
+    editComplete() {
+      const task = Task.parse(this.editText);
+      task.complete(!task.isComplete);
+      this.editText = task.stringify();
     },
 
     editOk() {
-      this.edit.task.load(this.edit.text);
+      this.editTask.load(this.editText);
+      if (this.editTask.index === -1) {
+        this.taskList.push(this.editTask);
+      }
       this.editCancel();
       this.update();
-      Application.isDialogActive = this.edit.show;
+      Application.isDialogActive = this.editShow;
     },
 
     editOpen(task) {
-      this.edit = {
-        show: true,
-        task: task,
-        text: task.stringify()
-      };
-      Application.isDialogActive = this.edit.show;
+      this.editShow = true;
+      this.editTask = task;
+      this.editText = task.stringify();
+      Application.isDialogActive = this.editShow;
+    },
+
+    editPriority(priority) {
+      const task = Task.parse(this.editText);
+      task.priority = priority;
+      this.editText = task.stringify();
     },
 
     load() {
